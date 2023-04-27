023f3942a645fdf3cc0cf74e12dd8039c1c77299
Isha Rahman <ishaarahman@gmail.com>
2023-04-26 22:18:04 -0400

    fixed navigation bug (screen freezing)

--- src/components/PortfolioContainer.jsx
+++ src/components/PortfolioContainer.jsx
@@ -22,50 +22,10 @@ export default function PortfolioContainer() {
 
     const handlePageChange = (page) => setCurrentPage(page);
 
-    const validName = (event) => {
-
-        const nameRegex = /^[a-z ,.'-]+$/i;
-        let name = event.target.value;
-        const p = document.getElementById('nameInput');
-
-        if (!name) {
-            p.innerHTML = 'This field cannot be empty.';
-        } else if (nameRegex.test(name) !== true) {
-            p.innerHTML = 'Invalid name entry.'
-        } else {
-            p.innerHTML = ''
-        }
-    };
-
-    const validEmail = (event) => {
-
-        const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-        let email = event.target.value;
-        const p = document.getElementById('emailInput');
-
-        if (!email) {
-            p.innerHTML = 'This field cannot be empty.';
-        } else if (emailRegex.test(email) !== true) {
-            p.innerHTML = 'Invalid email entry.'
-        } else {
-            p.innerHTML = ''
-        }
-    };
-
-    const submitForm = (event) => {
-        event.preventDefault();
-
-        let name = event.target.parentNode.children[0];
-        let email = event.target.parentNode.children[2];
-
-        name.value = '';
-        email.value = '';
-    }
-
     return (
         <div>
             <Navigation currentPage={currentPage} handlePageChange={handlePageChange} />
-            <Contact validName={validName} validEmail={validEmail} submitForm={submitForm} />
+            {/* <Contact /> */}
             {renderPage()}
         </div>
     );
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,12 +1,51 @@
 import React from 'react';
 import './Contact.scss';
 
-const Contact = ({ validName, validEmail, submitForm }) => {
+const Contact = () => {
+
+    const validName = (event) => {
+
+        const nameRegex = /^[a-z ,.'-]+$/i;
+        let name = event.target.value;
+        const p = document.getElementById('nameInput');
+
+        if (!name) {
+            p.innerHTML = 'This field cannot be empty.';
+        } else if (nameRegex.test(name) !== true) {
+            p.innerHTML = 'Invalid name entry.'
+        } else {
+            p.innerHTML = ''
+        }
+    };
+
+    const validEmail = (event) => {
+
+        const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+        let email = event.target.value;
+        const p = document.getElementById('emailInput');
+
+        if (!email) {
+            p.innerHTML = 'This field cannot be empty.';
+        } else if (emailRegex.test(email) !== true) {
+            p.innerHTML = 'Invalid email entry.'
+        } else {
+            p.innerHTML = ''
+        }
+    };
+
+    const submitForm = (event) => {
+        event.preventDefault();
+
+        let name = event.target.parentNode.children[0];
+        let email = event.target.parentNode.children[2];
+
+        name.value = '';
+        email.value = '';
+    }
 
     return (
 
         <>
-
             <section className='contact'>
                 <section className='contactForm'>
 
@@ -23,7 +62,8 @@ const Contact = ({ validName, validEmail, submitForm }) => {
                             id="submit"
                             type="submit"
                             value="SUBMIT"
-                            onClick={submitForm}></input>
+                            onClick={submitForm}
+                        ></input>
 
                     </form>
                 </section>
