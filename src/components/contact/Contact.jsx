a186a9a0f0187191201d397049d5b107f4ace173
Isha Rahman <ishaarahman@gmail.com>
2023-04-26 22:06:53 -0400

    contact styling and form validation completed

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
 import './Contact.scss';
 
-const Contact = () => {
+const Contact = ({ validName, validEmail, submitForm }) => {
+
     return (
-        <div>Contact</div>
+
+        <>
+
+            <section className='contact'>
+                <section className='contactForm'>
+
+                    <header>CONTACT ME</header>
+
+                    <form id="form">
+
+                        <input id="name" type="text" placeholder="NAME" onBlur={validName}></input>
+                        <p id='nameInput'></p>
+                        <input id="email" type="text" placeholder="E-MAIL" onBlur={validEmail}></input>
+                        <p id='emailInput'></p>
+                        <textarea id="message" type="text" placeholder="MESSAGE"></textarea>
+                        <input
+                            id="submit"
+                            type="submit"
+                            value="SUBMIT"
+                            onClick={submitForm}></input>
+
+                    </form>
+                </section>
+            </section>
+
+        </>
     )
 }
 
